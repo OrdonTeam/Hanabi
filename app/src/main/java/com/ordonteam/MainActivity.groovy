47d6f3f87e96a1e93b614e45c913a464132ffee7
ordon <ordon1313@gmail.com>
2014-11-06 09:07:52 +0100

    Groovy4Android

---
+++ app/src/main/java/com/ordonteam/MainActivity.groovy
@@ -0,0 +1,24 @@
+package com.ordonteam
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        LinearLayout linearLayout = new LinearLayout(this)
+        linearLayout.setOrientation(LinearLayout.VERTICAL)
+
+        TextView view = new TextView(this)
+        view.setText('Hello Groovy')
+        linearLayout.addView(view)
+
+        setContentView(linearLayout)
+    }
+}
---
+++ robospock/src/test/groovy/com/ordonteam/MainActivitySpec.groovy
@@ -0,0 +1,11 @@
+package com.ordonteam.daruziek
+
+import pl.polidea.robospock.RoboSpecification
+
+class MainActivitySpec extends RoboSpecification {
+
+    def "Robospock should run"() {
+        expect:
+            1==1
+    }
+}
