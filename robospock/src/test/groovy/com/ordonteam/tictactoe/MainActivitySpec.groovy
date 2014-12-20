dd705e0d4c7367e688fb8f1e55053fb0dc3a5c8d
ordon <ordon1313@gmail.com>
2014-12-21 00:45:13 +0100

    Added test to Swiss Knife

--- robospock/src/test/groovy/com/ordonteam/tictactoe/MainActivitySpec.groovy
+++ robospock/src/test/groovy/com/ordonteam/tictactoe/MainActivitySpec.groovy
@@ -1,11 +1,24 @@
 package com.ordonteam.tictactoe
 
+import org.robolectric.Robolectric
 import pl.polidea.robospock.RoboSpecification
 
 class MainActivitySpec extends RoboSpecification {
 
-    def "Robospock should run"() {
+//    def "Swiss Knife Test"() {
+//        given:
+//        def builder = Robolectric.buildActivity(MainActivity)
+//
+//        when:
+//        MainActivity mainActivity = builder.create().get()
+//
+//        then:
+//        mainActivity.helloText != null
+//        mainActivity.helloText.text == 'Hello Swiss Knife'
+//    }
+
+    def "Name"() {
         expect:
-            1==1
+        1==1
     }
 }
