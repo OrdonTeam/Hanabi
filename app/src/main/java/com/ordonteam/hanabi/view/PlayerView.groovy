c32632f0e76d2eeffee7249e806d64846440b255
pawel <pawelredzynski@gmail.com>
2015-01-03 13:29:19 +0100

    PlayerView add Player Image and NameFirstLetter

--- app/src/main/java/com/ordonteam/hanabi/view/PlayerView.groovy
+++ app/src/main/java/com/ordonteam/hanabi/view/PlayerView.groovy
@@ -2,11 +2,26 @@ package com.ordonteam.hanabi.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
+import com.ordonteam.hanabi.R
 
 
 class PlayerView extends LinearLayout {
-    PlayerView(Context context, AttributeSet attrs) {
-        super(context, attrs)
+    private ImageView playerImage
+    private TextView nameFirstLetter
+    PlayerView(Context context) {
+        super(context)
+
+        playerImage = new ImageView(context)
+        playerImage.setImageResource(R.drawable.ic_launcher)
+        addView(playerImage)
+
+        nameFirstLetter = new TextView(context)
+        nameFirstLetter.setTextSize(TypedValue.COMPLEX_UNIT_SP,40)
+        nameFirstLetter.setText("P")
+        addView(nameFirstLetter)
     }
 }
