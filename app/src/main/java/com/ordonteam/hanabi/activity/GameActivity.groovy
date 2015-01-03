7cbe55906f30189637a237140fca912df43e3c15
ghost <kas_kon5@wp.pl>
2015-01-03 13:08:08 +0100

    gameActivity leaving match onBackBtn pressed

--- app/src/main/java/com/ordonteam/hanabi/activity/GameActivity.groovy
+++ app/src/main/java/com/ordonteam/hanabi/activity/GameActivity.groovy
@@ -23,30 +23,31 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 @InjectContentView(R.layout.game_layout)
-class GameActivity extends AbstractGamesActivity implements OnTurnBasedMatchUpdateReceivedListener{
+class GameActivity extends AbstractGamesActivity implements OnTurnBasedMatchUpdateReceivedListener {
 
     @InjectView(R.id.turn)
     Button turn;
 
     private TurnBasedMatchConfig config
     private String invId
+    private TurnBasedMatch match
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
         invId = intent.getStringExtra(Multiplayer.EXTRA_INVITATION)
-        if(!invId)
+        if (!invId)
             config = GameConfig.configFromIntent(intent)
     }
 
     @Override
     void onConnected(Bundle bundle) {
         if (invId) {
-            Games.TurnBasedMultiplayer.acceptInvitation(client,invId).setResultCallback(this.&initiateMatchResult)
-            Games.TurnBasedMultiplayer.registerMatchUpdateListener(client,this)
+            Games.TurnBasedMultiplayer.acceptInvitation(client, invId).setResultCallback(this.&initiateMatchResult)
+            Games.TurnBasedMultiplayer.registerMatchUpdateListener(client, this)
         } else {
             Games.TurnBasedMultiplayer.createMatch(client, config).setResultCallback(this.&initiateMatchResult)
-            Games.TurnBasedMultiplayer.registerMatchUpdateListener(client,this)
+            Games.TurnBasedMultiplayer.registerMatchUpdateListener(client, this)
         }
     }
 
@@ -63,6 +64,7 @@ class GameActivity extends AbstractGamesActivity implements OnTurnBasedMatchUpda
             Games.TurnBasedMultiplayer.takeTurn(client, result.match.matchId, 'first'.bytes, next)
                     .setResultCallback(this.&updateMatchResult)
         }
+        match = result.match
     }
 
     private String nextPlayerId(TurnBasedMatch match) {
@@ -101,6 +103,7 @@ class GameActivity extends AbstractGamesActivity implements OnTurnBasedMatchUpda
     }
 
     void updateMatchResult(TurnBasedMultiplayer.UpdateMatchResult result) {
+        Log.e('updateMatchResult','update')
         Log.e("status", "result=${result.status}")
         Log.e("status", "match=${result.match?.status}")
         Log.e("status", "turn=${result.match?.turnStatus}")
@@ -118,13 +121,18 @@ class GameActivity extends AbstractGamesActivity implements OnTurnBasedMatchUpda
         })
     }
 
-   @Override
+    @Override
     void onTurnBasedMatchReceived(TurnBasedMatch match) {
         Log.e("status", "onTurnBasedMatchReceived")
         Log.e("status", "match=${match?.status}")
         Log.e("status", "turn=${match?.turnStatus}")
         if (match?.data)
             Log.e("status", "data=${new String(match.data)}")
+
+        if( match?.status == TurnBasedMatch.MATCH_STATUS_CANCELED ){
+            super.onBackPressed()
+        }
+
         String next = nextPlayerId(match)
         turn.setOnClickListener({
             Games.TurnBasedMultiplayer.takeTurn(client, match.matchId, 'next turn'.bytes, next).setResultCallback(this.&updateMatchResult)
@@ -137,4 +145,15 @@ class GameActivity extends AbstractGamesActivity implements OnTurnBasedMatchUpda
     void onTurnBasedMatchRemoved(String s) {
         Log.e("onTurnBasedMatchRemoved", "onTurnBasedMatchRemoved")
     }
+
+    @Override
+    void onBackPressed() {
+        if( match.getTurnStatus() == TurnBasedMatch.MATCH_TURN_STATUS_MY_TURN){
+            Games.TurnBasedMultiplayer.leaveMatchDuringTurn(client, match.getMatchId(), nextPlayerId(match))
+        }else{
+            Games.TurnBasedMultiplayer.leaveMatch(client,match.getMatchId())
+        }
+        super.onBackPressed()
+    }
+
 }
