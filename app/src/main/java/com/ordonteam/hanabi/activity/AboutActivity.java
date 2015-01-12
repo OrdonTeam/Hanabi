8ff263bbeda853d0f7dd3480dcf2c7a7a811980a
pawel <pawelredzynski@gmail.com>
2015-01-12 14:10:53 +0100

    Remove close button in aboutActivity

--- app/src/main/java/com/ordonteam/hanabi/activity/AboutActivity.java
+++ app/src/main/java/com/ordonteam/hanabi/activity/AboutActivity.java
@@ -3,8 +3,6 @@ package com.ordonteam.hanabi.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.ordonteam.hanabi.R;
@@ -22,12 +20,6 @@ public class AboutActivity extends Activity {
                 "wokatorek\nhttps://github.com/wokatorek\n" +
                 "daruziek\nhttps://github.com/daruziek";
         tvAbout.setText(tvAboutContent);
-        Button bClose = (Button) findViewById(R.id.bClose);
-        bClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
     }
 }
