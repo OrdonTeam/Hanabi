e849e0dccd91514170ee841d44d9d5812d788cf6
ordon <ordon1313@gmail.com>
2015-01-15 14:02:46 +0100

    separate user and players cards

--- app/src/main/java/com/ordonteam/hanabi/activity/HowToActivity.groovy
+++ app/src/main/java/com/ordonteam/hanabi/activity/HowToActivity.groovy
@@ -11,6 +11,7 @@ import com.ordonteam.hanabi.view.GameInfoView
 import com.ordonteam.inject.InjectActivity
 import com.ordonteam.inject.InjectContentView
 import com.ordonteam.inject.InjectView
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 
 @CompileStatic
@@ -20,26 +21,30 @@ class HowToActivity extends InjectActivity{
     @InjectView(R.id.how_to_row)
     LinearLayout row
 
+    @InjectView(R.id.how_to_row_2)
+    LinearLayout row2
+
     @InjectView(R.id.how_to_game)
     GameInfoView gameInfoView
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle)
-        CardView card0 = row.getChildAt(0) as CardView
-        card0.setUserCard(new HanabiCard(CardColor.RED,CardValue.FIVE))
-        CardView card1 = row.getChildAt(1) as CardView
-        card1.setUserCard(new HanabiCard(CardColor.RED,CardValue.FIVE,true,false))
-        CardView card2 = row.getChildAt(2) as CardView
-        card2.setUserCard(new HanabiCard(CardColor.RED,CardValue.FIVE,false,true))
-        CardView card3 = row.getChildAt(3) as CardView
-        card3.setPlayerCard(new HanabiCard(CardColor.RED,CardValue.FIVE))
-        CardView card4 = row.getChildAt(4) as CardView
-        card4.setPlayerCard(new HanabiCard(CardColor.RED,CardValue.FIVE,true,false))
-        CardView card5 = row.getChildAt(5) as CardView
-        card5.setPlayerCard(new HanabiCard(CardColor.RED,CardValue.FIVE,false,true))
-
+        initCards(row, 'setUserCard')
+        initCards(row2, 'setPlayerCard')
         gameInfoView.setRemainingCardsLeft(25);
         gameInfoView.setTopRejectedCard(new HanabiCard(CardColor.RED,CardValue.FIVE))
+    }
+
+    @CompileDynamic
+    private static void initCards(LinearLayout row, String method) {
+        CardView card0 = row.getChildAt(0) as CardView
+        card0."${method}"(new HanabiCard(CardColor.RED, CardValue.FIVE))
+        CardView card1 = row.getChildAt(1) as CardView
+        card1."${method}"(new HanabiCard(CardColor.RED, CardValue.FIVE, true, false))
+        CardView card2 = row.getChildAt(2) as CardView
+        card2."${method}"(new HanabiCard(CardColor.RED, CardValue.FIVE, false, true))
+        CardView card3 = row.getChildAt(3) as CardView
+        card3."${method}"(new HanabiCard(CardColor.RED, CardValue.FIVE, true, true))
     }
 }
