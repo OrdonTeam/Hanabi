16cc66e63eeb3c0e81c8f793b43ab3830fdd2c29
ordon <ordon1313@gmail.com>
2015-01-15 14:26:53 +0100

    nice about

--- app/src/main/java/com/ordonteam/hanabi/activity/AboutActivity.groovy
+++ app/src/main/java/com/ordonteam/hanabi/activity/AboutActivity.groovy
@@ -1,38 +1,55 @@
-package com.ordonteam.hanabi.activity;
+package com.ordonteam.hanabi.activity
 
-
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import android.text.util.Linkify
+import android.widget.ImageView
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ordonteam.hanabi.R;
 import com.ordonteam.inject.InjectActivity;
 import com.ordonteam.inject.InjectContentView;
-import com.ordonteam.inject.InjectView;
+import com.ordonteam.inject.InjectView
+import groovy.transform.CompileStatic;
 
+@CompileStatic
 @InjectContentView(R.layout.about)
-public class AboutActivity extends InjectActivity {
+class AboutActivity extends InjectActivity {
 
-    @InjectView(R.id.tvAbout)
-    private TextView tvAbout;
+    @InjectView(R.id.about_content)
+    LinearLayout content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String tvAboutContent =
-"""OrdonTeam
-https://github.com/OrdonTeam
-ghostbuster91
-https://github.com/ghostbuster91
-olabeee
-https://github.com/olabeee
-pared
-https://github.com/pared
-wokatorek
-https://github.com/wokatorek
-daruziek
-https://github.com/daruziek""";
-        tvAbout.setText(tvAboutContent);
+        add('OrdonTeam')
+        add('ghostbuster91')
+        add('olabeee')
+        add('pared')
+        add('wokatorek')
+        add('daruziek')
 
+    }
+
+    void add(String login) {
+        LinearLayout user = new LinearLayout(this)
+        user.setOrientation(LinearLayout.HORIZONTAL)
+        ImageView icon = new ImageView(this)
+        icon.setImageResource(R.drawable.ic_launcher)
+        user.addView(icon)
+
+        LinearLayout about = new LinearLayout(this)
+        about.setOrientation(LinearLayout.VERTICAL)
+
+        TextView loginView = new TextView(this)
+        loginView.setText(login)
+        about.addView(loginView)
+        TextView urlView = new TextView(this)
+        urlView.setAutoLinkMask(Linkify.ALL)
+        urlView.setText("https://github.com/$login")
+        about.addView(urlView)
+
+        user.addView(about)
+        content.addView(user)
     }
 }
